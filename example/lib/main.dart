a0ba9fe998b1ea2189e5650f0b581d6c32b549b5
hungrymonkey <boringmonkey@gmail.com>
2017-09-13 20:03:16 -0700

    i might have to redo this plugin.
    Why am i getting Error creating AudioRecord instance: initialization check failed with status -1 on previously test code?

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:save_wav/save_wav.dart';
+import 'dart:typed_data';
+import 'dart:async';
 
+import 'package:mic_stream/mic_stream.dart';
 void main() {
   runApp(new MyApp());
 }
@@ -13,34 +16,50 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
+  List<StreamSubscription<dynamic>> _micStreamSubscription = <StreamSubscription<dynamic>>[];
+  Uint16List _micAudioFragment= null;
+  num _samples = 10000;
+  num _clipCounter;
 
+  num _counter = 0;
+  List<int> _micClip = null;
   @override
   initState() {
     super.initState();
-    initPlatformState();
+    _micClip = new List();
+    _micAudioFragment = new Uint16List(0);
+    _micStreamSubscription.add(micEvents.listen((MicEvent e){
+      setState((){
+        _micAudioFragment = e.audioData;
+        if(_clipCounter < _samples) {
+          _micClip.addAll(_micAudioFragment);
+          _clipCounter++;
+        }
+      });
+    }));
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await SaveWav.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
+  @override
+  void dispose() {
+    super.dispose();
+    for (var subscription in _micStreamSubscription) {
+      subscription.cancel();
     }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
+  }
+  _saveClip() async {
+    try {
+      await SaveWav.saveWavUInt16('${_counter}.wav', _micClip, 48000, 1);
+    } on PlatformException {
+      
+    }
     if (!mounted)
       return;
 
     setState(() {
-      _platformVersion = platformVersion;
+      _counter++;
+      _micClip.clear();
     });
   }
-
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -49,7 +68,12 @@ class _MyAppState extends State<MyApp> {
           title: new Text('Plugin example app'),
         ),
         body: new Center(
-          child: new Text('Running on: $_platformVersion\n'),
+          child: new Text('Mic fragment size: ${_micClip.length}\n File saved: ${_counter}'),
+        ),
+        floatingActionButton: new FloatingActionButton(
+          onPressed: _saveClip,
+          tooltip: 'save to wav',
+          child: new Icon(Icons.save),
         ),
       ),
     );
